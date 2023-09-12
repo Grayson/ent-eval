8ebaa8af0a61839dbdae3d4f043acc94d05944e5
Grayson Hansard <grayson.hansard@gmail.com>
2023-09-12 12:32:32 -0400

    Add Edge to Todo

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 )
 
@@ -28,5 +29,9 @@ func (Todo) Fields() []ent.Field {
 
 // Edges of the Todo.
 func (Todo) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("parent", Todo.Type).
+			Unique().
+			From("children"),
+	}
 }
