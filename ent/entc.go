f587c379736a621d43f191b43b173fac6bd4b3a8
Grayson Hansard <grayson.hansard@gmail.com>
2023-09-14 15:47:30 -0400

    Implement server status endpoint

--- ent/entc.go
+++ ent/entc.go
@@ -10,11 +10,12 @@ import (
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 	"github.com/ogen-go/ogen"
+	"github.com/ogen-go/ogen/gen/ir"
 )
 
 func main() {
 	spec := new(ogen.Spec)
-	oas, err := entoas.NewExtension(entoas.Spec(spec))
+	oas, err := entoas.NewExtension(entoas.Spec(spec), entoas.Mutations(mutations()...))
 	if err != nil {
 		log.Fatalf("creating entoas extension: %v", err)
 	}
@@ -27,5 +28,24 @@ func main() {
 	err = entc.Generate("./schema", &gen.Config{}, entc.Extensions(ogent, oas))
 	if err != nil {
 		log.Fatalf("runnint ent codegen: %v", err)
+	}
+}
+
+func mutations() []entoas.MutateFunc {
+	return []entoas.MutateFunc{
+		func(g *gen.Graph, s *ogen.Spec) error {
+			okResponse := ogen.NewResponse().SetDescription("Server is alive and well").
+				AddContent(ir.EncodingTextPlain.String(), ogen.NewSchema().SetType("string"))
+			operation := ogen.NewOperation().
+				SetOperationID("ServerStatus").
+				SetSummary("Get information about the server").
+				AddResponse("200", okResponse).
+				AddResponse("503", ogen.NewResponse().SetDescription("Server is unavailable"))
+			path := ogen.NewPathItem().
+				SetDescription("Returns basic data about the server").
+				SetGet(operation)
+			s.AddPathItem("/status", path)
+			return nil
+		},
 	}
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"strings"
 
 	"entgo.io/ent/dialect"
 	"github.com/Grayson/ent-eval/ent"
@@ -12,6 +13,18 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type handler struct {
+	*ogent.OgentHandler
+}
+
+// Status implements ogent.Handler.
+func (handler) ServerStatus(ctx context.Context) (ogent.ServerStatusRes, error) {
+	status := ogent.ServerStatusOK{
+		Data: strings.NewReader("We're okay"),
+	}
+	return &status, nil
+}
 
 func main() {
 	client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
@@ -28,7 +41,9 @@ func main() {
 
 	seed(client, ctx)
 
-	server, err := ogent.NewServer(ogent.NewOgentHandler(client))
+	server, err := ogent.NewServer(handler{
+		OgentHandler: ogent.NewOgentHandler(client),
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
